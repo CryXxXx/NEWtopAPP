6db779892fc72ecdccef18c9ec264e0d0acff305
CryXxXx <wertywerty2324@gmail.com>
2025-01-21 22:46:35 +0700

    Add files via upload

---
+++ darts.js
@@ -0,0 +1,180 @@
+
+
+
+document.addEventListener('dragstart', (e) => {
+    e.preventDefault(); // Отключает перетаскивание
+});
+
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault(); // Отключает контекстное меню
+});
+
+// Отключение выделения при долгом нажатии на мобильных устройствах
+document.addEventListener('touchstart', (e) => {
+    e.preventDefault(); // Предотвращает выделение
+});
+
+
+
+
+
+
+
+const RepPage = document.getElementById('back-lobby-button');
+RepPage.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+
+
+
+
+const movingCircle = document.getElementById('movingCircle');
+const filled = document.getElementById('filled');
+const sliderContainer = document.querySelector('.slider-container');
+
+let currentValueIndex = 0;
+let isDragging = false;
+const totalMovementWidth = 81.5; // Общая длина передвижения кружка в vw
+const increment = totalMovementWidth / 6; // Увеличение на одну шестую часть от 81.5vw
+
+function updateSlider() {
+    const leftPosition = (currentValueIndex / 6) * totalMovementWidth; // Используем 81.5vw вместо ширины контейнера
+
+    movingCircle.style.left = `${leftPosition}vw`; // Устанавливаем позицию кружка
+    filled.style.width = `${(1.5 + currentValueIndex * increment)}vw`; // Обновляем ширину заполненной области
+}
+
+function getCurrentValueIndex(mouseX) {
+    const totalWidth = sliderContainer.offsetWidth;
+    const newIndex = Math.round((mouseX / totalWidth) * 6); // Изменено на 6
+    return Math.max(0, Math.min(newIndex, 6)); // Изменено на 6
+}
+
+movingCircle.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    document.body.style.cursor = 'grabbing';
+});
+
+document.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        const rect = sliderContainer.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        currentValueIndex = getCurrentValueIndex(mouseX);
+        updateSlider();
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    document.body.style.cursor = 'default';
+});
+
+// Инициализация состояния
+updateSlider();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let activeOption = null;
+let allDef = true; // Флаг, чтобы отслеживать состояние всех опций
+
+function changeImage(selectedOption) {
+    const defImage = selectedOption.querySelector('.def');
+    const supImage = selectedOption.querySelector('.sup');
+
+    // Если есть активный блок, и это не тот же самый блок
+    if (activeOption && activeOption !== selectedOption) {
+        const activeDefImage = activeOption.querySelector('.def');
+        const activeSupImage = activeOption.querySelector('.sup');
+
+        // Возвращаем активный блок к состоянию def
+        activeDefImage.style.opacity = '1';
+        activeSupImage.style.opacity = '0';
+    }
+
+    // Анимация для выбранного блока
+    if (supImage.style.opacity === '0') {
+        defImage.style.opacity = '0';
+        supImage.style.opacity = '1';
+        activeOption = selectedOption; // Устанавливаем активный блок
+        allDef = false; // Устанавливаем флаг, что не все опции в состоянии def
+    } else {
+        // Если уже активен, возвращаем его обратно
+        defImage.style.opacity = '1';
+        supImage.style.opacity = '0';
+        activeOption = null; // Сбрасываем активный блок
+        allDef = true; // Устанавливаем флаг, что все опции теперь в состоянии def
+    }
+
+    // Управление состоянием bet-button
+    toggleBetButton();
+}
+
+function toggleBetButton() {
+    const betButtonDef = document.querySelector('.bet-button .def');
+    const betButtonSup = document.querySelector('.bet-button .sup');
+
+    if (allDef) {
+        betButtonDef.style.opacity = '1';
+        betButtonSup.style.opacity = '0';
+    } else {
+        betButtonDef.style.opacity = '0';
+        betButtonSup.style.opacity = '1';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ replenishment.js
@@ -0,0 +1,144 @@
+document.addEventListener('dragstart', (e) => {
+    e.preventDefault(); // Отключает перетаскивание
+});
+
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault(); // Отключает контекстное меню
+});
+
+// Отключение выделения при долгом нажатии на мобильных устройствах
+document.addEventListener('touchstart', (e) => {
+    e.preventDefault(); // Предотвращает выделение
+});
+
+/*
+function openWinthdraw(){
+    const tg_id = window.Telegram.WebApp.WebappInitData.user.id
+
+    fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' // Указываем тип контента
+            },
+            body: JSON.stringify({ 'tg_id': tg_id }) // Преобразуем объект в JSON
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Преобразуем ответ в JSON
+        })
+        .then(data => {
+    window.Telegram.WebApp.openTelegramLink(data.check_url);
+    })
+        .catch((error) => {
+            console.error('Ошибка:', error);
+        });
+}
+
+
+*/
+
+const RepPage = document.getElementById('back-lobby-button');
+RepPage.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+
+
+
+const DepBut = document.getElementById('deposit');
+let isAnimating = false; // Счетчик для отслеживания состояния анимации
+
+DepBut.addEventListener('click', () => {
+    openDeposit(); // Ваша существующая функция
+
+    if (isAnimating) return; // Если анимация уже идет, выходим из функции
+    isAnimating = true; // Устанавливаем состояние анимации в true
+
+    setTimeout(() => {
+
+        const errorDiv = document.createElement('div');
+        errorDiv.className = 'error-cont';
+        errorDiv.id = 'error';
+        errorDiv.innerHTML = '<img src="assets/window-error.png" alt="Image" class="error-window">';
+        document.body.appendChild(errorDiv);
+        
+        setTimeout(() => {
+            anime({
+                targets: errorDiv,
+                translateY: ['15vw', '0'], // Движение вниз на 15vw
+                duration: 400,
+                opacity: [0, 1],
+                easing: 'easeInOutQuad',
+                complete: () => {
+
+                    setTimeout(() => {
+                        anime({
+                            targets: errorDiv,
+                            translateY: ['0', '15vw'], // Возвращение на исходную позицию
+                            duration: 400,
+                            opacity: [1, 0],
+                            easing: 'easeInOutQuad',
+                            complete: () => {
+                                errorDiv.remove();
+                                isAnimating = false;
+                            }
+                        });
+                    }, 2000);
+                }
+            });
+        }, 1500);
+    }, 0);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+function openDeposit() {
+    window.Telegram.WebApp.openTelegramLink("http://t.me/send?start=IVTGIPBCeUDC");
+}
+
+
+
+
+
+// Функция для анимации элементов с помощью Anime.js
+const showElements = () => {
+    anime({
+        targets: ['.replenish-cont', '.withdraw-cont'],
+        translateY: ['24.6vw', '0'],
+        opacity: [0, 1], // Изменение прозрачности
+        duration: 400, // Длительность анимации
+        easing: 'easeOutExpo', // Тип easing
+    });
+    vibrateDevice(200)
+};
+
+function vibrateDevice(duration) {
+        if ("vibrate" in navigator) {
+            navigator.vibrate(duration);
+        } else {
+            console.log("Вибрация не поддерживается на этом устройстве.");
+        }
+} 
+  
+
+
+
+
+
+
+
+
+
+window.addEventListener('load', showElements);
