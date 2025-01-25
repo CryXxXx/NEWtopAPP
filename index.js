2aa1ab6cc3d6a1e8320608380ed67e6ceab2833d
CryXxXx <wertywerty2324@gmail.com>
2025-01-26 00:30:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function when_page_start() {
 
 function place_coef() {
     
-    fetch("http://127.0.0.1:5000/get_last_game", {
+    fetch("https://tesgdl-176-118-32-227.ru.tuna.am/get_last_game", {
         method: 'GET', // Указываем метод GET
         headers: {
             'Content-Type': 'application/json' // Указываем тип контента (необязательно для GET-запроса)
